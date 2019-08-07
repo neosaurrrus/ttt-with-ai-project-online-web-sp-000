43ae0b897ab8ef0d42138be2823365c49c55bde4
Lukie Kang <lukiek+f@gmail.com>
2019-08-07 21:24:30 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,10 +1,7 @@
 module Players
   class Computer < Player
 
-    def valid_move?(board)
 
-      binding.pry
-    end
 
 
     def move(board)
@@ -16,9 +13,40 @@ module Players
         end
         input+=1
       end
-      valid_moves[0]
+      determine_move(valid_moves)
     end # of move
 
+    def determine_move(valid_moves)
+      chosen_move = rand(valid_moves.length)
+      #if its the first move
+        if board.turn_count == 0
+          return 5
+      #if its the 2nd move (opponent starting)
+        elsif board.turn_count == 1
+          if board.valid_move?(4)
+            return 5
+          elsif board.valid_move?(1)
+            return 1
+          else
+            return 3
+          end  
+          # check for near win conditions (self)
+        elsif board.turn_count > 1
+          my_win_conditions
+          if !my_win_conditions
+            thier_win_conditions
+          end
+        
+       end # of big logic
+      
+      
+      
+
+      # check for near win conditions (opponent)
+      
+      # evaluate win condition to persue
+      
+      
 
   end #of class
 end # of module
